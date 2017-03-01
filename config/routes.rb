f5f86e29799aba0b499b871e79fb611f97818824
linzhewei <zp8106@gmail.com>
2017-03-01 15:57:43 +0800

    update root / delete welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "welcome#index"
+  root "teams#index"
 
   resources :teams, only: [:index, :new, :show, :create] do
     resources :projects, only: [:show, :new, :create]
